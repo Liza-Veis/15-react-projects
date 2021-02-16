8efc0606048ca91fd2b39ccd3cf709e5ff758e75
Liza Veis <liza.veis14@gmail.com>
2021-02-17 01:48:47 +0200

    feat: complete stripe-submenus project

--- 13-stripe-submenus/src/Sidebar.js
+++ 13-stripe-submenus/src/Sidebar.js
@@ -1,9 +1,40 @@
-import React from 'react'
-import { FaTimes } from 'react-icons/fa'
-import sublinks from './data'
+import React from "react";
+import { FaTimes } from "react-icons/fa";
+import { useGlobalContext } from "./context";
+import sublinks from "./data";
 
 const Sidebar = () => {
-  return <h2>sidebar component</h2>
-}
+  const { isSidebarOpen, closeSidebar } = useGlobalContext();
+  return (
+    <aside className={`sidebar-wrapper ${isSidebarOpen && "show"}`}>
+      <div className="sidebar">
+        <button className="close-btn" onClick={closeSidebar}>
+          <FaTimes />
+        </button>
+        <div className="sidebar-links">
+          {sublinks.map((item, idx) => {
+            const { links, page } = item;
+            return (
+              <article key={idx}>
+                <h4>{page}</h4>
+                <div className="sidebar-sublinks">
+                  {links.map((link, idx) => {
+                    const { url, icon, label } = link;
+                    return (
+                      <a key={idx} href={url}>
+                        {icon}
+                        {label}
+                      </a>
+                    );
+                  })}
+                </div>
+              </article>
+            );
+          })}
+        </div>
+      </div>
+    </aside>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
--- 13-stripe-submenus/src/Submenu.js
+++ 13-stripe-submenus/src/Submenu.js
@@ -1,7 +1,44 @@
-import React, { useState, useRef, useEffect } from 'react'
+import React, { useState, useRef, useEffect } from "react";
+import { useGlobalContext } from "./context";
 
 const Submenu = () => {
-  return <h2>submenu component</h2>
-}
+  const {
+    isSubmenuOpen,
+    location,
+    page: { page, links },
+  } = useGlobalContext();
 
-export default Submenu
+  const container = useRef(null);
+  const [columns, setColumns] = useState(2);
+
+  useEffect(() => {
+    const submenu = container.current;
+    const { center, bottom } = location;
+    submenu.style.left = `${center}px`;
+    submenu.style.top = `${bottom}px`;
+    if (links.length <= 3) {
+      setColumns(links.length);
+    } else {
+      setColumns(4);
+    }
+  }, [location, links]);
+
+  return (
+    <aside className={`submenu ${isSubmenuOpen && "show"}`} ref={container}>
+      <h4>{page}</h4>
+      <div className={`submenu-center col-${columns}`}>
+        {links.map((link, idx) => {
+          const { label, icon, url } = link;
+          return (
+            <a href={url} key={idx}>
+              {icon}
+              {label}
+            </a>
+          );
+        })}
+      </div>
+    </aside>
+  );
+};
+
+export default Submenu;
